14705e85ee533e2a2b4eedf285793fac4f792754
Muslimbek Salimov <muslimbeksalimov@gmail.com>
2023-11-25 19:21:42 +0500

    our proekt 1.7.5

--- js/main.js
+++ js/main.js
@@ -2,10 +2,22 @@ let elButton = document.querySelector(".btn");
 let elButtonMenu = document.querySelector(".btn__menu");
 let elButtonExit = document.querySelector(".btn__exit");
 let elNav =document.querySelector(".nav");
+var elModal = document.querySelector(".modalka__info")
+var elBtn = docum.querySelector(".button__button")
+
+
+
 
 elButton.addEventListener("click", ()=> {
-
+    
     elButtonMenu.classList.toggle("menu__exit")
     elButtonExit.classList.toggle("exit__menu")
     elNav.classList.toggle("nav__blok")
+})
+   
+
+.elBtn.addEventListener("click", function (evt) {
+        evt.preventDefault();
+    
+        elModal.classList.toggle("modal__nav")
 })
